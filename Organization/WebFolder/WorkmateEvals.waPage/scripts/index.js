0092821cae40fbe3f18d03721971db20dc0661db
Alexandre Morgaut <alexandre.morgaut@gmail.com>
2014-01-28 17:40:32 +0100

    Publish Sources

---
+++ Organization/WebFolder/WorkmateEvals.waPage/scripts/index.js
@@ -0,0 +1,45 @@
+﻿
+WAF.onAfterInit = function onAfterInit() {// @lock
+
+// @region namespaceDeclaration// @startlock
+	var login1 = {};	// @login
+// @endregion// @endlock
+
+// eventHandlers// @lock
+
+	login1.logout = function login1_logout (event)// @startlock
+	{// @endlock
+		checkLogin();
+	};// @lock
+
+	login1.login = function login1_login (event)// @startlock
+	{// @endlock
+		checkLogin();
+	};// @lock
+	
+	function checkLogin()
+		{
+			var currUser = WAF.directory.currentUser();
+			if (currUser != null)
+			{
+				try {
+					source.workmate.allEntities();
+					$$('container3').show();
+				} catch (e) {
+					source.workmate.noEntities();
+				}
+				
+			}
+			else
+			{
+				source.workmate.noEntities();
+				$$('container3').hide();
+				$$('login1').showLoginDialog();
+
+			}
+		};
+// @region eventManager// @startlock
+	WAF.addListener("login1", "logout", login1.logout, "WAF");
+	WAF.addListener("login1", "login", login1.login, "WAF");
+// @endregion
+};// @endlock
---
+++ Organization/scripts/required.js
@@ -0,0 +1,31 @@
+﻿function myLogin(userName, password)
+{
+	//debugger;
+	var theEmployee = ds.Employee({login:userName});
+	if (theEmployee == null) // if no user was found
+	   return false; // let Wakanda try to find a user in the directory
+	else
+	{
+		// see of the stored hash value is correct
+		if (theEmployee.password == directory.computeHA1(userName, password)) 
+		{
+			var theTitle = '';
+			if (theEmployee.title != null)
+				theTitle = ', ' + theEmployee.title;
+			var myLoginInfo = {myEmployeeID: theEmployee.ID};
+			if (theEmployee.manager != null)				
+					myLoginInfo.myManagerID = theEmployee.manager.ID;
+			var theGroups = ['Employee'];
+			if (theEmployee.directReports.length > 0)
+				theGroups.push('Manager');
+			return {ID: theEmployee.ID, 
+					name: theEmployee.login, 
+					fullName: theEmployee.fullName + theTitle, 
+					belongsTo: theGroups,
+					storage : {loginInfo: myLoginInfo}
+					}
+		}
+		else
+			return { error: 1024, errorMessage:"invalid login" };
+	}
+};
